43d273e91f8fc73234f8b7717cd4d21f36be2575
Marek Zelinka <mzelinka17@gmail.com>
2023-02-17 15:00:16 +0100

    cleanup

--- index.js
+++ index.js
@@ -8,29 +8,6 @@ if (!port) {
   throw new Error("PORT must be set");
 }
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 const app = express();
 app.use(cors());
 app.use(express.json());
